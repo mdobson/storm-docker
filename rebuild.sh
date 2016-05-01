618fb56510f6a77e4b3deb017378870497083135
Matthew Dobson <mdobson4@gmail.com>
2016-05-01 09:03:22 -0400

    Updating build process to use my own images. Adds nodejs to storm processes.

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t="wurstmeister/storm" storm
-docker build -t="wurstmeister/storm-nimbus" storm-nimbus
-docker build -t="wurstmeister/storm-supervisor" storm-supervisor
-docker build -t="wurstmeister/storm-ui" storm-ui
+docker build -t="mdobs/storm" storm
+docker build -t="mdobs/storm-nimbus" storm-nimbus
+docker build -t="mdobs/storm-supervisor" storm-supervisor
+docker build -t="mdobs/storm-ui" storm-ui
